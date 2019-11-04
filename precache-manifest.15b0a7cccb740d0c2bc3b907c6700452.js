59a7a59fde37e4370e49d16a2819ef27309e8424
SvisT <svisteckij@gmail.com>
2019-11-04 02:00:13 +0200

    Updates

---
+++ precache-manifest.15b0a7cccb740d0c2bc3b907c6700452.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cf5a479634bf85d536d197be1dea12f",
+    "url": "/goit-react-hw-03-bank-account/index.html"
+  },
+  {
+    "revision": "81706a85faa3d8d0e067",
+    "url": "/goit-react-hw-03-bank-account/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "e8567a665ca87c1cf310",
+    "url": "/goit-react-hw-03-bank-account/static/css/main.af00b7b3.chunk.css"
+  },
+  {
+    "revision": "81706a85faa3d8d0e067",
+    "url": "/goit-react-hw-03-bank-account/static/js/2.5d2ab6d7.chunk.js"
+  },
+  {
+    "revision": "e8567a665ca87c1cf310",
+    "url": "/goit-react-hw-03-bank-account/static/js/main.1a56026c.chunk.js"
+  },
+  {
+    "revision": "5ae27dd2b4af87281072",
+    "url": "/goit-react-hw-03-bank-account/static/js/runtime-main.86f7a662.js"
+  }
+]);
